fedd34fb049baa8050c7a635da64e5ad19baebd3
rajakarthikeyan <47881815+rajakarthikeyan@users.noreply.github.com>
2019-06-07 10:09:51 +0530

    Update test 2.py

--- test 2.py
+++ test 2.py
@@ -1,6 +1,8 @@
 
 num = int(input("Enter a num: "))
 if (num % 5)==0:
+   print("{0} is odd.format(num)")
+elif(num % 4)==0:
    print("{0} is even.format(num)")
 else :
-   print("{0} is invalid.format(num)")
+   print("invalid")
